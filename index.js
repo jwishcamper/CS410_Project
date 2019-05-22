78bbbc66e08354d7afc51bc95b40091b55c3999a
Jonathan Wishcamper <j.wishcamper@bellevuecollege.edu>
2019-05-21 20:39:27 -0700

    Client Usernames
    Different clients now have different usernames, stored server-side.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var clients = [];
+var username = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,13 +10,19 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  clients.push(socket); 
+  username[clients.indexOf(socket)] = "Anonymous";
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message',username[clients.indexOf(socket)]+": "+ msg);
+  });
+  socket.on('namechange', function(chg){
+	  username[clients.indexOf(socket)] = chg;
   });
   socket.on('disconnect', function(){
+	clients.splice(clients.indexOf(socket), 1);
     console.log('user disconnected');
   });
 });
