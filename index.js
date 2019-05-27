40eb13762641ff2c72b02612272fb15283d19377
Jonathan Wishcamper <j.wishcamper@bellevuecollege.edu>
2019-05-27 12:31:57 -0700

    DB & QoL
    Added database connection functionality
    Window now scrolls down automatically when a message is recieved
    text box no longer blocks chat messages

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+/*
+ * Example SQL query:
+ * sqlcon.query("select username from userDB.userStorage where userID=1", function (err, result, fields) {
+		  if(err) console.log(err);
+		    console.log(result);
+		  });		  
+ * */
+
+
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var sql = require('mysql')
 var clients = [];
 var username = [];
+var sqlcon = sql.createConnection({
+	  host: "localhost",
+	  user: "student",
+	  password: "password"
+	});
+
+sqlcon.connect(function(err) {
+	if(err) console.log(err);
+	else console.log("Database connected.");
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -15,12 +36,15 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
+  
   socket.on('chat message', function(msg){
     io.emit('chat message',username[clients.indexOf(socket)]+": "+ msg);
   });
+  
   socket.on('namechange', function(chg){
 	  username[clients.indexOf(socket)] = chg;
   });
+  
   socket.on('disconnect', function(){
 	clients.splice(clients.indexOf(socket), 1);
     console.log('user disconnected');
