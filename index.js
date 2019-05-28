808458bc57305102b891b892b7790c480223f9bb
Jonathan Wishcamper <j.wishcamper@bellevuecollege.edu>
2019-05-28 14:50:53 -0700

    Login and Create account functionality
    added create account that connects with mysql database
    added login that automatically sets display username to accounts username

--- index.js
+++ index.js
@@ -41,6 +41,40 @@ io.on('connection', function(socket){
     io.emit('chat message',username[clients.indexOf(socket)]+": "+ msg);
   });
   
+  socket.on('loginid', function(loginid,pass){
+	    //query login and pass from database, if exists login and change user name
+	  sqlcon.query("select username from userDB.userStorage where username = \""+loginid+"\" and userpass = \""+pass+"\";", function (err, result, fields) {
+		  if(err) console.log(err);
+		  if(result[0] != null) {
+		  io.emit('loginsuccess');
+		    username[clients.indexOf(socket)] = result[0].username; }
+		  else{
+			  io.emit('loginfailure');
+		  }
+		  });		 
+	  });
+  
+  socket.on('submit', function(loginid,pass,email){
+	    //take user input to create an account in database
+	  if(loginid=="" || pass ==""){//if there's nothing in user or pass fields, stop here
+		  io.emit('createfailure');
+	  }
+	  else{
+		  sqlcon.query("select username from userDB.userStorage where username = \""+loginid+"\";", function (err, result, fields) {
+		  if(err) console.log(err);
+		  if(result[0] != null) {
+			  io.emit('createtaken'); //if this query has something, the username is taken
+		  }
+		  else{ //if not, go ahead and insert into database
+			  sqlcon.query("insert into userDB.userStorage values(default,\""+loginid+"\",\""+pass+"\",\""+email+"\",curdate(),0,null);", function (err, result, fields) {
+				  if(err) {console.log(err);  }
+				  else{ io.emit('createsuccess'); }
+				  });	
+		  }
+		  });
+	  }
+  });
+  
   socket.on('namechange', function(chg){
 	  username[clients.indexOf(socket)] = chg;
   });
