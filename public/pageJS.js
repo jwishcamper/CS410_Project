692c19d6e58207508ab338a7f43543ae78fcfff6
Jonathan Wishcamper <j.wishcamper@bellevuecollege.edu>
2019-06-03 17:43:17 -0700

    Server and DB code for friends list
    Add friend/populate friends methods for server side

--- public/pageJS.js
+++ public/pageJS.js
@@ -1,7 +1,8 @@
-//navigate to another html page using:   window.location.replace("page.html");
-
 $(function() { //wait for document to fully load before running javascript
       var socket = io();
+      
+      //contains a list of userID's of friends, updated every time populateFriends(); is called
+      var friends = [];
       
       //Cache DOM
       var $sendButton = $('#sendBut');
@@ -9,6 +10,8 @@ $(function() { //wait for document to fully load before running javascript
       var $messageWindow = $('#messages');
       
       socket.emit('loginsucceeded', localStorage.getItem('username'));
+      populateFriends(); //initial populate
+      
       //do this when send message button clicked
       $sendButton.on('click', function(e) { //send message
         e.preventDefault(); // prevents page reloading
@@ -18,10 +21,27 @@ $(function() { //wait for document to fully load before running javascript
       });
 
 
-      //these socket.on's are called by client for dialog boxes,chat messages, etc. Not directly used by client.
+      //these socket.on's are called by client for dialog boxes,chat messages, etc. Not directly called by client.
       socket.on('chat message', function(msg) { //receive message
         $messageWindow.append($('<li>').text(msg));
         $messageWindow.scrollTop($messageWindow[0].scrollHeight);
       });
-     
+      
+      //called when friend added successfully
+      socket.on('friendAdded', function() {
+    	  populateFriends();
+    	  alert("Friend added!"); 
+      });
+      
+      //called to populate friends list
+      socket.on('friendsList',function(name){
+    	  friends.push(name);
+      });
+      
+      //helper function to populate user's friends list
+      function populateFriends(){
+    	  friends.length=0;
+    	  socket.emit('populateFriends');
+      }
+      
     });
